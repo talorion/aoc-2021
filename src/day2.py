d9c884252af3a6adb0c2ebdb299e25b7066a9640
talorion <gregor.mayramhof@gmail.com>
2022-03-20 13:15:13 +0100

    second day success

--- src/day2.py
+++ src/day2.py
@@ -1,9 +1,46 @@
 def read_input_file(filename: str) -> list:
-    return list()
+    commands: list = []
+    f = open(filename, "r")
+    for el in f.readlines():
+        lst_el = el.strip().split(" ")
+        tup_el = tuple([lst_el[0], int(lst_el[1])])
+        commands.append(tup_el)
+    f.close()
+    return commands
 
-# use collections Counter to count number od occurences
-# count hashable objects with collections.Counter
+
+def move(commands: list) -> dict:
+    coordinates: dict = {"horizontal_position": 0, "depth": 0}
+    for command in commands:
+        if command[0] == "forward":
+            coordinates["horizontal_position"] += command[1]
+        elif command[0] == "down":
+            coordinates["depth"] += command[1]
+        elif command[0] == "up":
+            coordinates["depth"] -= command[1]
+    return coordinates
+
+
+def move_with_aim(commands: list) -> dict:
+    coordinates: dict = {"horizontal_position": 0, "depth": 0}
+    aim: int = 0
+    for command in commands:
+        if command[0] == "forward":
+            coordinates["horizontal_position"] += command[1]
+            coordinates["depth"] += command[1] * aim
+        elif command[0] == "down":
+            aim += command[1]
+        elif command[0] == "up":
+            aim -= command[1]
+    return coordinates
+
+
+def calculate_result(coordinates: dict) -> int:
+    return coordinates["horizontal_position"] * coordinates["depth"]
 
 
 if __name__ == '__main__':
-    report = read_input_file("data/day2.input")
+    commands: list = read_input_file("data/day2.input")
+    coordinates: dict = move_with_aim(commands)
+    result: int = calculate_result(coordinates)
+    print(result)
--- tst/test_day2.py
+++ tst/test_day2.py
@@ -1,26 +1,60 @@
+import unittest
+from src.day2 import read_input_file
+from src.day2 import move
+from src.day2 import calculate_result
+from src.day2 import move_with_aim
 
-# import unittest
-# from src.day2 import read_input_file
-# 
-# 
-# class TestDay1(unittest.TestCase):
-# 
-#     def __init__(self, *args, **kwargs):
-#         super(TestDay1, self).__init__(*args, **kwargs)
-#         self.__sample_file: str = "data/test_day2.input"
-#         self.__sample_directions = [
-#                                     ("forward", 5),
-#                                     ("down", 5),
-#                                     ("forward", 8),
-#                                     ("up", 3),
-#                                     ("down", 8),
-#                                     ("forward", 2)
-#                                    ]
-# 
-#     def test_read_input_file_returns_report(self):
-#         read_result: list = read_input_file(self.__sample_file)
-#         self.assertEqual(read_result, self.__sample_directions)
-# 
-# 
-# if __name__ == '__main__':
-#     unittest.main()
+
+class TestDay1(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TestDay1, self).__init__(*args, **kwargs)
+        self.__sample_file: str = "data/test_day2.input"
+        self.__sample_commands: list = [
+                                    ("forward", 5),
+                                    ("down", 5),
+                                    ("forward", 8),
+                                    ("up", 3),
+                                    ("down", 8),
+                                    ("forward", 2)
+                                   ]
+        self.__expected_coordinates: dict = {
+                                        "horizontal_position": 15,
+                                        "depth": 10
+                                        }
+        self.__expected_result: int = 150
+        self.__expected_coordinates_with_aim: dict = {
+                                        "horizontal_position": 15,
+                                        "depth": 60
+                                        }
+        self.__expected_result_with_aim: int = 900
+
+    def test_read_input_file_returns_report(self):
+        commands: list = read_input_file(self.__sample_file)
+        self.assertEqual(commands, self.__sample_commands)
+
+    def test_move(self):
+        commands: list = read_input_file(self.__sample_file)
+        coordinates: dict = move(commands)
+        self.assertEqual(coordinates, self.__expected_coordinates)
+
+    def test_result(self):
+        commands: list = read_input_file(self.__sample_file)
+        coordinates: dict = move(commands)
+        result: int = calculate_result(coordinates)
+        self.assertEqual(result, self.__expected_result)
+
+    def test_move_with_aim(self):
+        commands: list = read_input_file(self.__sample_file)
+        coordinates: dict = move_with_aim(commands)
+        self.assertEqual(coordinates, self.__expected_coordinates_with_aim)
+
+    def test_result_with_aim(self):
+        commands: list = read_input_file(self.__sample_file)
+        coordinates: dict = move_with_aim(commands)
+        result: int = calculate_result(coordinates)
+        self.assertEqual(result, self.__expected_result_with_aim)
+
+
+if __name__ == '__main__':
+    unittest.main()
