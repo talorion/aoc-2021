12a884cceeaef7e3c4675a7fe5ee0303a58f3fe0
talorion <gregor.mayramhof@gmail.com>
2022-04-02 20:39:28 +0200

    day4 success

--- src/day4.py
+++ src/day4.py
@@ -1,21 +1,171 @@
+import copy
+import math
+
+
+class Board:
+    def __init__(self, size: int = 0, data: list[int] = []):
+        self.size: int = size
+        self.data: list[int] = data
+        self.draw_numbers: list[int] = []
+
+    def __copy__(self):
+        obj = type(self).__new__(self.__class__)
+        obj.size = self.size
+        obj.data = self.data.copy()
+        obj.draw_numbers: list[int] = []
+        return obj
+
+    def __str__(self) -> str:
+        return f"{self.size} {self. data}"
+
+    def __eq__(self, other) -> bool:
+        if self.size != other.size:
+            return False
+        if self.data != other.data:
+            return False
+        return True
+
+    def clear(self) -> None:
+        self.size = 0
+        self.data.clear()
+
+    def add_data(self, raw_data: str) -> None:
+        strings = raw_data.strip().split()
+        self.data += [int(i.strip()) for i in strings]
+
+    def score(self) -> int:
+        score = 0
+        for candidate in self.data:
+            if candidate in self.draw_numbers:
+                continue
+            score += candidate
+        return score
+
+    def is_winner(self) -> bool:
+        marked_rows: list[int] = [0] * self.size
+        marked_colums: list[int] = [0] * self.size
+        for idx, candidate in enumerate(self.data):
+            if candidate in self.draw_numbers:
+                marked_rows[math.floor(idx / self.size)] += 1
+                marked_colums[idx % self.size] += 1
+            if self.size in marked_rows:
+                return True
+            if self.size in marked_colums:
+                return True
+        return False
+
+    def update(self, draw: int) -> None:
+        if self.is_winner():
+            return
+        self.draw_numbers += [draw]
+
+
 class PuzzleDay4:
     def __init__(self, filename: str):
         self.filename: str = filename
-        self.drw_num: str = ""
+        self.random_order: list[int] = []
+        self.boards: list[Board] = []
+        self.current_turn: int = 0
+        self.last_draw: int = None
+        self.winner_index: int = None
+        self.looser_index: int = None
+        self.winner_idexes: list[int] = []
+        self.winning_turn: int = None
+        self.loosing_turn: int = None
 
     def __str__(self) -> str:
         return ""
 
     def read_input_file(self):
         with open(self.filename, "r") as f:
-            self.drw_num = f.readline()
+            # fist line is the draw nubers
+            strings = f.readline().strip().split(",")
+            self.random_order = [int(i.strip()) for i in strings]
+
+            # empty line after draw numbers
+            next_line = f.readline()
+
+            idx: int = 0
+            board: Board = Board()
+            board.clear()
             while True:
                 next_line = f.readline()
+                stripped_next_line = next_line.strip()
+                # EOF
                 if not next_line:
+                    board.size = idx
+                    self.boards.append(copy.copy(board))
                     break
-                self.report.append(next_line.strip())
-        return self.report
+                if not stripped_next_line:
+                    board.size = idx
+                    self.boards.append(copy.copy(board))
+                    board.clear()
+                    idx = 0
+                    continue
+                board.add_data(stripped_next_line)
+                idx += 1
+        return self.boards
+
+    def winner(self) -> Board:
+        if self.winner_index is not None:
+            return self.boards[self.winner_index]
+        for idx, board in enumerate(self.boards):
+            if board.is_winner():
+                # self.winning_turn = self.current_turn
+                self.winner_index = idx
+                self.winner_idexes += [idx]
+                return board
+        return None
+
+    def looser(self) -> Board:
+        self.winner_index = None
+        number_of_winners = 0
+        max_winners = len(self.boards)
+        for idx, board in enumerate(self.boards):
+            if board.is_winner() and idx not in self.winner_idexes:
+                number_of_winners += 1
+                if not self.winning_turn:
+                    self.winning_turn = self.current_turn
+                self.winner_idexes += [idx]
+                self.winner_index = self.winner_idexes[0]
+            if len(self.winner_idexes) == max_winners:
+                if not self.loosing_turn:
+                    self.loosing_turn = self.current_turn
+                self.looser_index = self.winner_idexes[max_winners - 1]
+                return self.boards[self.looser_index]
+        return None
+
+    def update(self) -> None:
+        self.current_turn += 1
+        if len(self.random_order) < self.current_turn:
+            return
+        self.last_draw = self.random_order[self.current_turn - 1]
+        for board in self.boards:
+            board.update(self.last_draw)
+
+    def board_score(self, board: Board, winning_draw: int) -> int:
+        if not board:
+            return None
+        board_score = board.score()
+        if not board_score:
+            return None
+        return board_score * winning_draw
+
+    def winner_score(self) -> int:
+        board = self.boards[self.winner_idexes[0]]
+        winning_draw = self.random_order[self.winning_turn - 1]
+        return self.board_score(board, winning_draw)
+
+    def looser_score(self) -> int:
+        board = self.boards[self.winner_idexes[-1]]
+        loosing_draw = self.random_order[self.loosing_turn - 1]
+        return self.board_score(board, loosing_draw)
 
 
 if __name__ == "__main__":
-    pass
+    puzzle = PuzzleDay4("data/day4.input")
+    puzzle.read_input_file()
+    while not puzzle.looser():
+        puzzle.update()
+    print(f"winning score = {puzzle.winner_score()}")
+    print(f"loosing score = {puzzle.looser_score()}")
--- tst/test_day4.py
+++ tst/test_day4.py
@@ -1,11 +1,14 @@
+import copy
 import unittest
+from src.day4 import Board
 from src.day4 import PuzzleDay4
 
 
-class TestDay1(unittest.TestCase):
+class TestDay4(unittest.TestCase):
     def __init__(self, *args, **kwargs):
+        unittest.TestCase.__init__(self, *args, **kwargs)
         self.filename = "data/test_day4.input"
-        self.exp_random_order = [
+        self.exp_random_order: list[int] = [
             7,
             4,
             9,
@@ -34,94 +37,172 @@ class TestDay1(unittest.TestCase):
             26,
             1,
         ]
-        self.exp_boards[
-            [
-                22,
-                13,
-                17,
-                11,
-                0,
-                8,
-                2,
-                23,
-                4,
-                24,
-                21,
-                9,
-                14,
-                16,
-                7,
-                6,
-                10,
-                3,
-                18,
-                5,
-                1,
-                12,
-                20,
-                15,
-                19,
-            ],
-            [
-                3,
-                15,
-                0,
-                2,
-                22,
-                9,
-                18,
-                13,
-                17,
-                5,
-                19,
-                8,
-                7,
-                25,
-                23,
-                20,
-                11,
-                10,
-                24,
-                4,
-                14,
-                21,
-                16,
-                12,
-                6,
-            ],
-            [
-                14,
-                21,
-                17,
-                24,
-                4,
-                10,
-                16,
-                15,
-                9,
-                19,
-                18,
-                8,
-                23,
-                26,
-                20,
-                22,
-                11,
-                13,
-                6,
-                5,
-                2,
-                0,
-                12,
-                3,
-                7,
-            ],
-        ]
+        self.exp_boards = []
+        self.exp_boards.append(
+            copy.copy(
+                Board(
+                    5,
+                    [
+                        22,
+                        13,
+                        17,
+                        11,
+                        0,
+                        8,
+                        2,
+                        23,
+                        4,
+                        24,
+                        21,
+                        9,
+                        14,
+                        16,
+                        7,
+                        6,
+                        10,
+                        3,
+                        18,
+                        5,
+                        1,
+                        12,
+                        20,
+                        15,
+                        19,
+                    ],
+                )
+            )
+        )
+        self.exp_boards.append(
+            copy.copy(
+                Board(
+                    5,
+                    [
+                        3,
+                        15,
+                        0,
+                        2,
+                        22,
+                        9,
+                        18,
+                        13,
+                        17,
+                        5,
+                        19,
+                        8,
+                        7,
+                        25,
+                        23,
+                        20,
+                        11,
+                        10,
+                        24,
+                        4,
+                        14,
+                        21,
+                        16,
+                        12,
+                        6,
+                    ],
+                )
+            )
+        )
+        self.exp_boards.append(
+            copy.copy(
+                Board(
+                    5,
+                    [
+                        14,
+                        21,
+                        17,
+                        24,
+                        4,
+                        10,
+                        16,
+                        15,
+                        9,
+                        19,
+                        18,
+                        8,
+                        23,
+                        26,
+                        20,
+                        22,
+                        11,
+                        13,
+                        6,
+                        5,
+                        2,
+                        0,
+                        12,
+                        3,
+                        7,
+                    ],
+                )
+            )
+        )
+        self.test_rounds: int = 5
+        self.exp_turn: int = 12
+        self.exp_winner_index: int = 2
+        self.exp_winner_score: int = 4512
+        self.exp_looser_index: int = 1
+        self.exp_looser_score: int = 1924
 
     def test_read_input_file_returns_report(self):
         puzzle = PuzzleDay4(self.filename)
-        report = puzzle.read_input_file(self.filename)
-        self.assertEqual(report, self.exp_data)
+        boards = puzzle.read_input_file()
+        self.assertListEqual(boards, self.exp_boards)
+
+    def test_read_input_file_reads_random_order(self):
+        puzzle = PuzzleDay4(self.filename)
+        puzzle.read_input_file()
+        self.assertSequenceEqual(puzzle.random_order, self.exp_random_order)
+
+    def test_no_win_after_read(self):
+        puzzle = PuzzleDay4(self.filename)
+        puzzle.read_input_file()
+        self.assertIsNone(puzzle.looser())
+
+    def test_no_win_after_5_rounds(self):
+        puzzle = PuzzleDay4(self.filename)
+        puzzle.read_input_file()
+        while puzzle.current_turn < self.test_rounds:
+            puzzle.update()
+        self.assertIsNone(puzzle.looser())
+
+    def test_winner_after_12_rounds(self):
+        puzzle = PuzzleDay4(self.filename)
+        puzzle.read_input_file()
+        while not puzzle.looser():
+            puzzle.update()
+        self.assertEqual(puzzle.winning_turn, self.exp_turn)
+
+    def test_winner_is_third_board(self):
+        puzzle = PuzzleDay4(self.filename)
+        puzzle.read_input_file()
+        while not puzzle.looser():
+            puzzle.update()
+        self.assertEqual(puzzle.winner_index, self.exp_winner_index)
+
+    def test_winner_has_expected_score(self):
+        puzzle = PuzzleDay4(self.filename)
+        puzzle.read_input_file()
+        while not puzzle.looser():
+            puzzle.update()
+        self.assertEqual(puzzle.winner_score(), self.exp_winner_score)
+
+    def test_looser_is_second_board(self):
+        puzzle = PuzzleDay4(self.filename)
+        puzzle.read_input_file()
+        while not puzzle.looser():
+            puzzle.update()
+        self.assertEqual(puzzle.looser_index, self.exp_looser_index)
+
+    def test_looser_has_expected_score(self):
+        puzzle = PuzzleDay4(self.filename)
+        puzzle.read_input_file()
+        while not puzzle.looser():
+            puzzle.update()
+        self.assertEqual(puzzle.looser_score(), self.exp_looser_score)
 
 
 if __name__ == "__main__":
